8a75d91185c41e66e7a332c224508b9fa7e9bf30
Ivan Fonseca <ivanfon@riseup.net>
2020-04-09 01:17:50 -0400

    Prepare package for publishing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+declare global {
+  interface Props {
+    creator: string;
+    description: string;
+    locked: boolean;
+    state: 'open' | 'closed';
+    title: string;
+  }
+
+  interface IssueProps extends Props {}
+
+  interface PRProps extends Props {
+    branch: string;
+    files: string[];
+    isDraft: boolean;
+  }
+}
+
 export { creatorMatches } from './creatorMatches';
 export { descriptionMatches } from './descriptionMatches';
 export { isLocked } from './isLocked';
